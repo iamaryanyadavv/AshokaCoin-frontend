530a1d41e3587eaa9915a67a2cea140445757596
iamaryanyadavv <aryanyadav1601@gmail.com>
2023-12-05 23:58:00 +0530

    added faucet modal functionality again

--- src/pages/buyToken.js
+++ src/pages/buyToken.js
@@ -75,7 +75,6 @@ export default function BuyToken(props) {
                 theme: "dark",
             });
         } finally {
-            setShowFaucetModal(false)
             setTx_processing(false);
             setNo_Of_Tokens('0')
             props.onActionComplete()
@@ -134,6 +133,7 @@ export default function BuyToken(props) {
                 theme: "dark",
             });
         } finally {
+            setShowFaucetModal(false)
             setFaucet_tx_processing(false)
             setNo_Of_Tokens('0')
             props.onActionComplete()
